2d102e0a53d370396e7117e27e3c7201d9e81317
brobrigado <brobrigado@yahoo.com>
2026-02-04 21:56:05 -0800

    add registrants to event page

--- crud.js
+++ crud.js
@@ -2,9 +2,11 @@ class Crud {
     constructor(template) {
         this.template = template;
         this.eventsUrl = 'https://crudcrud.com/api/7e42b21784e849e197d7dcb5be768efe/events/';
+        this.registrantsUrl = 'https://crudcrud.com/api/7e42b21784e849e197d7dcb5be768efe/registrants/';
     }
 
     async viewEventList() {
+        this.template.clearAll();
         this.template.updateMessage('<div class="loading">Loading events...</div>');
 
         try {
@@ -25,11 +27,12 @@ class Crud {
         }
     }
 
-    async viewEvent(id) {
+    async viewEvent(eventId) {
+        this.template.clearAll();
         this.template.updateMessage('<div class="loading">Loading event...</div>');
 
         try {
-            const response = await fetch(`${this.eventsUrl}${id}`);
+            const response = await fetch(`${this.eventsUrl}${eventId}`);
             if (!response.ok) throw Error(response.message);
 
             const data = await response.json();
@@ -38,6 +41,31 @@ class Crud {
             this.template.clear('events');
 
             this.template.createEventCard(data, this);
+
+            this.viewRegistrants(eventId);
+
+        } catch (error) {
+            this.template.updateMessage(`<div class="error">Error: ${error.message}</div>`);
+        }
+    }
+
+    async viewRegistrants(eventId) {
+        this.template.updateMessage('<div class="loading">Loading registrants...</div>');
+
+        try {
+            const response = await fetch(this.registrantsUrl);
+            if (!response.ok) throw Error(response.message);
+
+            const data = await response.json();
+
+            this.template.clear('messages');
+            this.template.clear('registrants');
+
+            data.forEach(registrant => {
+                if(registrant.eventId == eventId) {
+                    this.template.createRegistrantListCard(registrant, this);
+                }
+            });
 
         } catch (error) {
             this.template.updateMessage(`<div class="error">Error: ${error.message}</div>`);
--- template.js
+++ template.js
@@ -3,6 +3,7 @@ class Template {
         this.messagesDiv = document.getElementById('messages');
         this.eventsDiv = document.getElementById('events');
         this.formsDiv = document.getElementById('forms');
+        this.registrantsDiv = document.getElementById('registrants');
     }
 
     clear(id) {
@@ -16,9 +17,19 @@ class Template {
             case 'forms':
                 this.formsDiv.replaceChildren();
                 break;
+            case 'registrants':
+                this.registrantsDiv.replaceChildren();
+                break;
             default:
                 break;
         }
+    }
+
+    clearAll() {
+        this.messagesDiv.replaceChildren();
+        this.eventsDiv.replaceChildren();
+        this.formsDiv.replaceChildren();
+        this.registrantsDiv.replaceChildren();
     }
 
     updateMessage(message) {
@@ -63,5 +74,34 @@ class Template {
         eventCard.innerHTML = html;
 
         this.eventsDiv.appendChild(eventCard);
+    }
+
+    createRegistrantListCard(registrant, crud) {
+        const registrantCard = document.createElement('div');
+        registrantCard.classList.add('registrant-card');
+
+        let html = 
+        `<h3>${registrant.fullName}</h3>
+        <p>name: ${registrant.eventTitle}</p>
+        <p>email: ${registrant.email}</p>
+        <p>guests: ${registrant.guests}</p>`;
+        if(registrant.notes) {
+            html += `<p>notes: ${registrant.notes}</p>`;
+        }
+        // html += `<button onclick="putRegistrant('${registrant._id}')">Update</button>`;
+        // html += `<button onclick="deleteRegistrant('${registrant._id}')">Delete</button>`;
+        registrantCard.innerHTML = html;
+
+        registrantCard.innerHTML = html;
+
+        const registrantUpdateButton = document.createElement('button');
+        registrantUpdateButton.textContent = 'Update';
+        registrantUpdateButton.addEventListener("click", () => {
+            console.log(registrant._id);
+            // crud.viewEvent(registrant._id);
+        });
+        registrantCard.appendChild(registrantUpdateButton);
+
+        this.registrantsDiv.appendChild(registrantCard);
     }
 }
